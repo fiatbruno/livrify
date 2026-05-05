bc78621dfac0807463438de53a9fdafa05d54317
Fiat Bruno <fiatbruno10@gmail.com>
2026-05-05 12:54:56 -0500

    Created the database and schemas

---
+++ database/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache ||(global.mongooseCache = { conn: null, promise: null });
+
+
+export const connectToDatabase = async () => { 
+    if (cached.conn) return cached.conn;
+
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {bufferCommands: false});
+    }
+    try{
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        console.error("MongoDB conneciton erro. Error connecting to database", error);
+        throw error;
+    }
+    console.info("MongoDB connected successfully");
+    return cached.conn;
+};
